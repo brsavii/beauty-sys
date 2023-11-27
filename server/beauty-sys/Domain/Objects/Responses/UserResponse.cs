40c853f2f30dbcf672308cfbea0865f4969c0931
Guilherme Machado Santana <guilherme.santana@plasson.com.br>
2023-11-27 17:33:30 -0300

    fix: adding required to user name

--- server/beauty-sys/Domain/Objects/Responses/UserResponse.cs
+++ server/beauty-sys/Domain/Objects/Responses/UserResponse.cs
@@ -2,7 +2,7 @@
 {
     public record UserResponse
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public DateTime InsertedAt { get; set; }
     }
 }
